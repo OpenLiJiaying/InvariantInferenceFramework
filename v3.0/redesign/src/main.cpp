df0c24e5211f0533b4476fe6817d6db4bd83c9c2
lijiaying@win7 <lijiaying1989@gmail.com>
2015-11-05 01:21:23 +0800

    divide svm-i form svm in main

--- v3.0/redesign/src/main.cpp
+++ v3.0/redesign/src/main.cpp
@@ -125,9 +125,7 @@ int main(int argc, char** argv)
 		if (passRat < 1) {
 			std::cout << " [FAIL] The problem is not linear separable.. Trying to solve is by SVM-I algo" << std::endl;
 			std::cerr << "*******************************USING SVM_I NOW******************************" << std::endl;
-			psvm = new SVM_I_algo(print_null);
-			rnd = 1;
-			goto init;
+			goto start_svm_i;
 		}
 		std::cout << " [PASS]" << std::endl;
 
@@ -209,10 +207,108 @@ int main(int argc, char** argv)
 	return 0;
 
 
+
+
+
+
+
+
 start_svm_i:
-	SVM_I_algo *psvmi = new SVM_I_algo(print_null);
-	psvmi->insertFromTraceSet<int>(&TS[POS]);
-	psvmi->insertFromTraceSet<int>(&TS[NEG]);
+	psvm = new SVM_I_algo(print_null);
+	goto istart;
+
+	for (rnd = 1; rnd <= max_iter; rnd++) {
+		TS[POS].reset();
+		TS[NEG].reset();
+		/*
+		*	The first round is very special, so we put this round apart with its following rounds.
+		*	1> We used random values as inputs for program executions in the first round.
+		*	2> We need to make sure there are at last two classes of generated traces. "positive" and "negative"
+		*/
+
+		std::cout << "[" << rnd << "]-----------------------------------------------------------------------------------------------------"
+			<< std::endl;
+		std::cout << "\t(1) running programs...[" << inputs_aft << "] {";
+		for (int i = 0; i < inputs_aft; i++) {
+			Equation::linearSolver(p, inputs);
+			std::cout << inputs << " | ";
+			run_target(inputs);
+		}
+
+		std::cout << "}" << std::endl;
+
+		if (TS[CNE].length > 0) {
+			std::cout << "Program BUG! Encountered a counter-example." << std::endl;
+			std::cout << &TS[CNE] << std::endl;
+			return -1;
+		}
+
+	istart:
+		std::cout << "\t(2) start training process...[";
+		std::cout << "+" << TS[POS].length << " | -" << TS[NEG].length << " | ";
+		psvm->insertFromTraceSet<int>(&TS[POS]);
+		psvm->insertFromTraceSet<int>(&TS[NEG]);
+		std::cout << psvm->size() << "]" << std::endl;
+		//	std::cout << psvm->problem.l << "]" << std::endl;
+		//	std::cout << &(psvm->problem) << std::endl;
+
+		psvm->train();
+		std::cout << "\t |-->> " << psvm; //<< std::endl;
+
+
+		/*
+		*	check on its own training data.
+		*	There should be no prediction errors.
+		*/
+		std::cout << "\t(3) checking on training traces.";
+		double passRat = psvm->predictOnProblem();
+		std::cout << " [" << passRat * 100 << "%]";
+		if (passRat < 1) {
+			std::cout << " [FAIL] The problem is not linear separable.. Trying to solve is by SVM-I algo" << std::endl;
+			std::cerr << "*******************************USING SVM_I NOW******************************" << std::endl;
+			psvm = new SVM_I_algo(print_null);
+			rnd = 1;
+			goto init;
+		}
+		std::cout << " [PASS]" << std::endl;
+
+
+		/*
+		*	Check on Question traces.
+		*	There should not exists one traces, in which a negative state is behind a positive state.
+		*/
+		std::cout << "\t(4) checking on Question traces.";
+		std::cout << " [" << TS[QST].length << "]";
+
+
+		//std::cout << "\t    ";
+		std::cout << " [PASS]" << std::endl;
+
+
+		/*
+		*	bCon is used to store the convergence check return value for the last time.
+		*	We only admit convergence if the three consecutive round are converged.
+		*	This is to prevent in some round the points are too right to adjust the classifier.
+		*/
+		std::cout << "\t(5) check convergence: ";
+		if (psvm->equation->isSimilar(p) == 0) {
+			if (bCon == true) {
+				std::cout << "[TT] \t[SUCCESS] rounding off" << std::endl;
+				break;
+			}
+			std::cout << "[FT]";
+			bCon = true;
+		}
+		else {
+			std::cout << ((bCon == true) ? "[T" : "[F") << "F] ";
+			bCon = false;
+		}
+		std::cout << "\t [FAIL] neXt round " << std::endl;
+		if (p != NULL) {
+			delete p;
+		}
+		p = psvm->equation;
+	}
 
 
 	return 0;
