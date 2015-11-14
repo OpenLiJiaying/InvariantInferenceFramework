0b8c43114de8f23f2222c27bbec9fdbe9b4ed10c
lijiaying@workstation <i@jiaying.li>
2015-11-14 23:23:35 +0800

    update

--- include/color.h
+++ include/color.h
@@ -3,56 +3,7 @@
 #include <iostream>
 
 enum color {RED = 0, YELLOW, GREEN, BLUE, WHITE};
-
-#ifdef WIN32
-#include <windows.h>
-	void set_console_color(std::ostream& out, int choice = YELLOW) {
-		HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-		switch (choice) {
-		case RED: // red
-			SetConsoleTextAttribute(hOut, FOREGROUND_RED | FOREGROUND_INTENSITY);
-			break;
-		case YELLOW: // yellow
-			SetConsoleTextAttribute(hOut, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-			break;
-		case GREEN: // green
-			SetConsoleTextAttribute(hOut, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-			break;
-		case BLUE: // blue
-			SetConsoleTextAttribute(hOut, FOREGROUND_BLUE | FOREGROUND_INTENSITY);
-			break;
-		case WHITE: // white
-			SetConsoleTextAttribute(hOut, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-			break;
-		}
-	}
-#endif
-
-
-#ifdef linux
-	void set_console_color(std::ostream& out, int choice = YELLOW) {
-		switch (choice) {
-		case RED: // red
-			out << "\033[33;\x1b[31m";
-			break;
-		case YELLOW: // yellow
-			out << "\033[33;\x1b[33m";
-			break;
-		case GREEN: // green
-			out << "\033[33;\x1b[32m";
-			break;
-		case BLUE: // blue
-			out << "\033[33;\x1b[34m";
-			break;
-		case WHITE: // white
-			out << "\033[0m";
-			break;
-		}	
-	}
-#endif
-
-	void unset_console_color(std::ostream& out) {
-		set_console_color(out, WHITE);
-	}
+void set_console_color(std::ostream& out, int choice = YELLOW); 
+void unset_console_color(std::ostream& out);
 
 #endif
--- include/svm.h
+++ include/svm.h
@@ -1,132 +1,7 @@
-#ifndef _LIBSVM_H_
-#define _LIBSVM_H_
-#include "header.h"
-#include <iostream>
-//#include <stdarg.h>
-//#include "float.h"
-//#include "color.h"
-#define LIBSVM_VERSION 320
+#ifndef _SVM_H_
+#define _SVM_H_
+#include "svm_core.h"
 
-extern int libsvm_version;
-
-
-struct svm_node
-{
-	double value;
-
-
-	friend std::ostream& operator << (std::ostream& out, const svm_node* sn) {
-		out << sn->value;
-		return out;
-	}
-};
-
-struct svm_problem
-{
-	int l;
-	double *y;
-	struct svm_node **x;
-
-	friend std::ostream& operator << (std::ostream& out, const svm_problem& sp) {
-		for (int i = 0; i < sp.l; i++) {
-			out << sp.y[i] << "(" << (sp.x[i][0]).value;
-			for (int j = 1; j < VARS; j++)
-				out << ", " << (sp.x[i][j]).value;
-			out << ") ";
-		}
-		return out;
-	}
-};
-
-
-
-enum { C_SVC, NU_SVC, ONE_CLASS, EPSILON_SVR, NU_SVR };	/* svm_type */
-enum { LINEAR, POLY, RBF, SIGMOID, PRECOMPUTED }; /* kernel_type */
-
-struct svm_parameter
-{
-	int svm_type;
-	int kernel_type;
-	int degree;	/* for poly */
-	double gamma;	/* for poly/rbf/sigmoid */
-	double coef0;	/* for poly/sigmoid */
-
-	/* these are for training only */
-	double cache_size; /* in MB */
-	double eps;	/* stopping criteria */
-	double C;	/* for C_SVC, EPSILON_SVR and NU_SVR */
-	int nr_weight;		/* for C_SVC */
-	int *weight_label;	/* for C_SVC */
-	double* weight;		/* for C_SVC */
-	double nu;	/* for NU_SVC, ONE_CLASS, and NU_SVR */
-	double p;	/* for EPSILON_SVR */
-	int shrinking;	/* use the shrinking heuristics */
-	int probability; /* do probability estimates */
-};
-
-//
-// svm_model
-// 
-struct svm_model
-{
-	struct svm_parameter param;	/* parameter */
-	int nr_class;		/* number of classes, = 2 in regression/one class svm */
-	int l;			/* total #SV */
-	struct svm_node **SV;		/* SVs (SV[l]) */
-	double **sv_coef;	/* coefficients for SVs in decision functions (sv_coef[k-1][l]) */
-	double *rho;		/* constants in decision functions (rho[k*(k-1)/2]) */
-	double *probA;		/* pariwise probability information */
-	double *probB;
-	int *sv_indices;        /* sv_indices[0,...,nSV-1] are values in [1,...,num_traning_data] to indicate SVs in the training set */
-
-	/* for classification only */
-
-	int *label;		/* label of each class (label[k]) */
-	int *nSV;		/* number of SVs for each class (nSV[k]) */
-	/* nSV[0] + nSV[1] + ... + nSV[k-1] = l */
-	/* XXX */
-	int free_sv;		/* 1 if svm_model is created by svm_load_model*/
-	/* 0 if svm_model is created by svm_train */
-};
-
-struct svm_model *svm_train(const struct svm_problem *prob, const struct svm_parameter *param);
-void svm_cross_validation(const struct svm_problem *prob, const struct svm_parameter *param, int nr_fold, double *target);
-
-int svm_save_model(const char *model_file_name, const struct svm_model *model);
-struct svm_model *svm_load_model(const char *model_file_name);
-
-int svm_get_svm_type(const struct svm_model *model);
-int svm_get_nr_class(const struct svm_model *model);
-void svm_get_labels(const struct svm_model *model, int *label);
-void svm_get_sv_indices(const struct svm_model *model, int *sv_indices);
-int svm_get_nr_sv(const struct svm_model *model);
-double svm_get_svr_probability(const struct svm_model *model);
-
-double svm_predict_values(const struct svm_model *model, const struct svm_node *x, double* dec_values);
-double svm_predict(const struct svm_model *model, const struct svm_node *x);
-double svm_predict_probability(const struct svm_model *model, const struct svm_node *x, double* prob_estimates);
-
-void svm_free_model_content(struct svm_model *model_ptr);
-void svm_free_and_destroy_model(struct svm_model **model_ptr_ptr);
-void svm_destroy_param(struct svm_parameter *param);
-
-const char *svm_check_parameter(const struct svm_problem *prob, const struct svm_parameter *param);
-int svm_check_probability_model(const struct svm_model *model);
-
-void svm_set_print_string_function(void (*print_func)(const char *));
-
-int svm_model_visualization(const svm_model *model, Equation* equ);
-
-void print_svm_samples(const svm_problem *sp);
-
-struct svm_model *svm_I_train(const struct svm_problem *prob, const struct svm_parameter *param);
-
-
-//#ifdef __cplusplus
-//}
-//#endif
-
-//void print_null(const char *s) {}
 class SVM // : public ClassifyAlgo
 {
 	public:
@@ -137,175 +12,26 @@ class SVM // : public ClassifyAlgo
 		double training_label[max_items * 2];
 		double* training_set[max_items * 2];
 
-		SVM(void (*f) (const char*) = NULL) {
-			problem.l = 0;
+		SVM(void (*f) (const char*) = NULL);
 
-			main_equation = NULL;
-			model = NULL;
+		~SVM(); 
 
-			param.svm_type = C_SVC;
-			param.kernel_type = LINEAR;
-			param.degree = 3;
-			param.gamma = 0;	// 1/num_features
-			param.coef0 = 0;
-			param.nu = 0.5;
-			param.cache_size = 100;
-			//	param.C = 1;
-			//param.C = DBL_MAX;
-			param.C = 1000;
-			param.eps = 1e-3;
-			param.p = 0.1;
-			param.shrinking = 1;
-			param.probability = 0;
-			param.nr_weight = 0;
-			param.weight_label = NULL;
-			param.weight = NULL;
-			if (f != NULL)
-				svm_set_print_string_function(f);
+		virtual int prepare_training_data(States* gsets, int& pre_positive_size, int& pre_negative_size);
 
-			for (int i = 0; i < 2 * max_items; i++)
-				training_label[i] = -1;
-			problem.x = (svm_node**)(training_set);
-			problem.y = training_label;
-		}
+		virtual int train();
 
-		~SVM() {
-			if (model != NULL)
-				delete model;
-			//if (problem.y != NULL)
-			//	delete problem.y;
-			// here we should check x[i] for each.
-			// be careful about whether it is imported from double trace set or int trace set.
-			// these two cases should be handled separatly.
-		}
+		virtual double predict_on_training_set();
 
-		virtual int prepare_training_data(States* gsets, int& pre_positive_size, int& pre_negative_size) {
-			int cur_positive_size = gsets[POSITIVE].size();
-			int cur_negative_size = gsets[NEGATIVE].size();
+		virtual int check_question_set(States& qset);
 
-			std::cout << "+[";
-			std::cout << cur_positive_size - pre_positive_size << "|";
-			std::cout << cur_negative_size - pre_negative_size << "";
-			std::cout << "] ==> [";
-			std::cout << cur_positive_size << "+|";
-			std::cout << cur_negative_size << "-";
-			std::cout << "]";
+		friend std::ostream& operator << (std::ostream& out, const SVM& svm);
 
-			//double** training_set = (double**)(problem.x);
-			//double* training_label = (double*)(problem.y);
-			// prepare new training data set
-			// training set & label layout:
-			// data :  0 | positive states | negative states | ...
-			// label:    | 1, 1, ..., 1, . | -1, -1, ..., -1, -1, -1, ...
-			// move the negative states from old OFFSET: [pre_positive_size] to new OFFSET: [cur_positive_size]
-			memmove(training_set + cur_positive_size, training_set + pre_positive_size, pre_negative_size * sizeof(double*));
-			// add new positive states at OFFSET: [pre_positive_size]
-			for (int i = pre_positive_size; i < cur_positive_size; i++) {
-				training_set[i] = gsets[POSITIVE].values[i];
-				training_label[i] = 1;
-			}
-			// add new negative states at OFFSET: [cur_positive_size + pre_negative_size]
-			for (int i = pre_negative_size; i < cur_negative_size; i++) {
-				training_set[cur_positive_size + i] = gsets[NEGATIVE].values[i];
-			}
-			problem.l = cur_positive_size + cur_negative_size;
-			pre_positive_size = cur_positive_size;
-			pre_negative_size = cur_negative_size;
-			return 0;
-		}
+		virtual int size();
 
-
-		virtual int train() {
-			if (problem.y == NULL || problem.x == NULL)
-				return -1;
-			const char* error_msg = svm_check_parameter(&problem, &param);
-			if (error_msg) {
-				std::cout << "ERROR: " << error_msg << std::endl;
-				exit(-1);
-			}
-			model = svm_train(&problem, &param);
-			/*for (int i = 0; i < 100000; i++)
-				if (sin(i) + cos(i) > 1.414)
-					std::cout << ".";
-					*/
-			main_equation = new Equation();
-			svm_model_visualization(model, main_equation);
-			svm_free_and_destroy_model(&model);
-			return 0;
-		}
-
-
-		virtual double predict_on_training_set() {
-			if (problem.l <= 0) return 0;
-			int pass = 0;
-			for (int i = 0; i < problem.l; i++) {
-				pass += (Equation::calc(main_equation, (double*)problem.x[i]) * problem.y[i] > 0) ? 1 : 0;
-			}
-			return static_cast<double>(pass) / problem.l;
-		}
-
-
-		virtual int check_question_set(States& qset) {
-			if (main_equation == NULL) return -1;
-			std::cout << " [" << qset.traces_num() << "]";
-			for (int i = 0; i < qset.p_index; i++) {
-				int pre = -1, cur = 0;
-				std::cout << ".";
-				//std::cout << "\t\t" << i << ">";
-				//gsets[QUESTION].print_trace(i);
-				for (int j = qset.index[i]; j < qset.index[i + 1]; j++) {
-					cur = Equation::calc(main_equation, qset.values[j]);
-					//std::cout << ((cur >= 0) ? "+" : "-");
-					if ((pre > 0) && (cur < 0)) {
-						// deal with wrong question trace.
-						// Trace back to print out the whole trace and the predicted labels.
-						std::cerr << "\t\t[FAIL]\n \t  Predict wrongly on Question traces." << std::endl;
-						qset.print_trace(i);
-						for (int j = qset.index[i]; j < qset.index[i + 1]; j++) {
-							cur = Equation::calc(main_equation, qset.values[j]);
-							std::cout << ((cur >= 0) ? "+" : "-");
-						}
-						std::cout << std::endl;
-						return -1;
-					}
-					pre = cur;
-				}
-				//std::cout << "END" << std::endl;
-			}
-			std::cout << " [PASS]";
-			return 0;
-		}
-
-
-		friend std::ostream& operator << (std::ostream& out, const SVM& svm) {
-			out << "SVM: ";
-			out << *svm.main_equation; // << std::endl;
-			return out;
-		}
-
-
-		virtual int size()
-		{
-			return problem.l;
-		}
-
-
-		virtual Equation* roundoff(int& num)
-		{
-			num = 1;
-			Equation* equs = new Equation[1];
-			main_equation->roundoff(equs[0]);
-			return equs;
-		}
+		virtual Equation* roundoff(int& num);
 
 	private:
-		virtual int predict(double* v) {
-			if (main_equation == NULL) return -2;
-			if (v == NULL) return -2;
-			double res = Equation::calc(main_equation, v);
-			if (res >= 0) return 1;
-			else return -1;
-		}
+		virtual int predict(double* v);
 };
 
-#endif /* _LIBSVM_H */
+#endif /* _SVM_H */
